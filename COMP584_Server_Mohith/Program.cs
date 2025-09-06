0adc3ba2defcb8800b418a4bbe4a786179b9bc28
mk504221 <mk504221@ECS1538-10>
2025-09-05 17:27:13 -0700

    Add project files.

---
+++ COMP584_Server_Mohith/Program.cs
@@ -0,0 +1,33 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "My API", Version = "v1" });
+    // Optional: Include XML comments for better documentation
+    // var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    // var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    // c.IncludeXmlComments(xmlPath);
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+    options.SwaggerEndpoint("/swagger/v1/swagger.json","My API V1");
+    });
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
