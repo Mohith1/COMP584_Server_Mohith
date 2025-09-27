5f99dc90acbc3c225ac17fb71b43336834026cf7
Mohith1k <mohith81999@gmail.com>
2025-09-26 18:04:21 -0700

    Migrations

---
+++ WorldModel/Migrations/20250927005456_Initial.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WorldModel.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Country",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NAME = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    ISO2 = table.Column<string>(type: "char(2)", unicode: false, fixedLength: true, maxLength: 2, nullable: false),
+                    ISO3 = table.Column<string>(type: "char(3)", unicode: false, fixedLength: true, maxLength: 3, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Country", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "City",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    COUNTRY_ID = table.Column<int>(type: "int", nullable: false),
+                    CITY_NAME = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    LATITUDE = table.Column<double>(type: "float", nullable: false),
+                    LONGITUDE = table.Column<double>(type: "float", nullable: false),
+                    POPULATION = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_City", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_City_Country",
+                        column: x => x.COUNTRY_ID,
+                        principalTable: "Country",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_City_COUNTRY_ID",
+                table: "City",
+                column: "COUNTRY_ID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "City");
+
+            migrationBuilder.DropTable(
+                name: "Country");
+        }
+    }
+}
