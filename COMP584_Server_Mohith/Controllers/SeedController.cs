96461250d7e903b111841e209c0db75a840f5e25
Mohith1k <mohith81999@gmail.com>
2025-10-03 17:44:15 -0700

    Seed Controller

---
+++ COMP584_Server_Mohith/Controllers/SeedController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WorldModel;
+
+namespace COMP584_Server_Mohith.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SeedController(Comp584MohithDatabaseContext context) : ControllerBase
+    {
+        // POST: api/Countries
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult> PostCountries()
+        {
+           
+            await context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+    }
+}
