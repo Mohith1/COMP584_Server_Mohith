b66fa894295bf3e452dd2af967df9cb6664181ae
Mohith1k <mohith81999@gmail.com>
2025-10-10 15:57:33 -0700

    Load data

--- COMP584_Server_Mohith/Controllers/SeedController.cs
+++ COMP584_Server_Mohith/Controllers/SeedController.cs
@@ -1,19 +1,22 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WorldModel;
 
 namespace COMP584_Server_Mohith.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class SeedController(Comp584MohithDatabaseContext context) : ControllerBase
+    public class SeedController(Comp584MohithDatabaseContext context,IHostEnvironment environment) : ControllerBase
     {
         // POST: api/Countries
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        string path = Path.Combine(environment.ContentRootPath, "Data/worldcities.csv");
         [HttpPost ("Countries")]
         public async Task<ActionResult> PostCountries()
         {
-           
+
+            var countries = await context.Countries.ToDictionaryAsync(c => c.Name, StringComparer.OrdinalIgnoreCase);
             await context.SaveChangesAsync();
 
             return Ok();
