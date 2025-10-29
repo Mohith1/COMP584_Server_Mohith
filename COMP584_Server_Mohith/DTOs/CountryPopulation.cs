6a3d9e1dc76e8a0edf9667e729490ea38b9a5973
Mohith Kanthamneni <50568915+Mohith1@users.noreply.github.com>
2025-10-29 11:46:32 -0700

    Update CountryPopulation.cs

--- COMP584_Server_Mohith/DTOs/CountryPopulation.cs
+++ COMP584_Server_Mohith/DTOs/CountryPopulation.cs
@@ -1,1 +1,11 @@
-
+namespace COMP584_SERVER.DTOs
+{
+    public class CountryPopulation
+    {
+        public required string Name { get; set; }
+        public int Id { get; set; }
+        public required string Iso2 { get; set; }
+        public required string Iso3 { get; set; }
+        public decimal Population { get; set; }
+    }
+}
